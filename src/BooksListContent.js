980e8ee65b3a33f982918c27e52cb894c21aa23b
Sebastian Reuter <sebastian.reuter@live.de>
2020-11-17 20:42:03 +0100

    refactor: reduce code duplication

--- src/BooksListContent.js
+++ src/BooksListContent.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import BookShelf from './BookShelf'
 
-const BooksListContent = (props) => (
-    <div className="list-books-content">
-        <div>
-            <BookShelf
-                title="Currently Reading"
-                books={props.books.filter(book => book.shelf === 'currentlyReading')} 
-                handleShelfChanged={props.handleShelfChanged} />
-            <BookShelf
-                title="Want to Read"
-                books={props.books.filter(book => book.shelf === 'wantToRead')} 
-                handleShelfChanged={props.handleShelfChanged} />
-            <BookShelf
-                title="Read"
-                books={props.books.filter(book => book.shelf === 'read')}
-                handleShelfChanged={props.handleShelfChanged} />
+const BooksListContent = (props) => {
+    const shelfs = [
+        { title: 'Currently Reading', name: 'currentlyReading' },
+        { title: 'Want to Read', name: 'wantToRead' },
+        { title: 'Read', name: 'read' }
+    ];
+    return (
+        <div className="list-books-content">
+            <div>
+                {shelfs.map(shelf => (
+                    <BookShelf 
+                        key={shelf.name}
+                        title={shelf.title}
+                        books={props.books.filter(book => book.shelf === shelf.name)}
+                        handleShelfChanged={props.handleShelfChanged} />
+                ))}
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default BooksListContent;
